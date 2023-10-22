bf070893699646d7eede1c1afd43d511c9d089c3
SanaUllahRasheed123 <starsreachersacademy@gmail.com>
2023-10-22 20:17:11 +0500

    listupdated

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -1,3 +1,55 @@
+// import React, { Component } from "react";
+// import "./TodoApp.css";
+
+// class TodoApp extends Component {
+//   constructor() {
+//     super();
+//     this.state = {
+//       todos: [],
+//       task: "",
+//     };
+//   }
+
+//   handleTaskChange = (event) => {
+//     this.setState({ task: event.target.value });
+//   };
+
+//   addTask = () => {
+//     if (this.state.task) {
+//       this.setState((prevState) => ({
+//         todos: [...prevState.todos, prevState.task],
+//         task: "",
+//       }));
+//     }
+//   };
+
+//   render() {
+//     return (
+//       <div className="todo-app">
+//         <h1>Todo List</h1>
+//         <div className="todo-input">
+//           <input
+//             type="text"
+//             placeholder="Enter a task"
+//             value={this.state.task}
+//             onChange={this.handleTaskChange}
+//           />
+//           <button onClick={this.addTask}>Add</button>
+//         </div>
+//         <ul>
+//           {this.state.todos.map((todo, index) => (
+//             <li key={index}>{todo}</li>
+//           ))}
+//         </ul>
+//       </div>
+//     );
+//   }
+// }
+
+// export default TodoApp;
+
+
+
 import React, { Component } from "react";
 import "./TodoApp.css";
 
@@ -23,6 +75,15 @@ class TodoApp extends Component {
     }
   };
 
+  removeTask = (index) => {
+    const updatedTodos = this.state.todos.filter((_, i) => i !== index);
+    this.setState({ todos: updatedTodos });
+  };
+
+  removeAllTasks = () => {
+    this.setState({ todos: [] });
+  };
+
   render() {
     return (
       <div className="todo-app">
@@ -38,9 +99,15 @@ class TodoApp extends Component {
         </div>
         <ul>
           {this.state.todos.map((todo, index) => (
-            <li key={index}>{todo}</li>
+            <li key={index}>
+              {todo}
+              <button onClick={() => this.removeTask(index)}>Remove</button>
+            </li>
           ))}
         </ul>
+        {this.state.todos.length > 0 && (
+          <button onClick={this.removeAllTasks}>Remove All</button>
+        )}
       </div>
     );
   }
