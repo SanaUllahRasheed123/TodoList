af0433de63de13f7a2911b3703b4f19fc6570e3e
SanaUllahRasheed123 <starsreachersacademy@gmail.com>
2023-10-22 20:46:04 +0500

    UpdFunCom

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -116,83 +116,83 @@
 // export default TodoApp;
 
 
-import React, { Component } from "react";
+// 
+
+
+import React, { useState } from "react";
 import "./TodoApp.css";
 
-class TodoApp extends Component {
-  constructor() {
-    super();
-    this.state = {
-      todos: [],
-      task: "",
-    };
-  }
+const TodoApp = () => {
+  const [task, setTask] = useState("");
+  const [todos, setTodos] = useState([]);
 
-  handleTaskChange = (event) => {
-    this.setState({ task: event.target.value });
+  const handleTaskChange = (event) => {
+    setTask(event.target.value);
   };
 
-  addTask = () => {
-    if (this.state.task) {
-      this.setState((prevState) => ({
-        todos: [...prevState.todos, { task: prevState.task, isCompleted: false }],
-        task: "",
-      }));
+  const addTask = () => {
+    if (task) {
+      setTodos([...todos, { task, isCompleted: false }]);
+      setTask("");
     }
   };
 
-  removeTask = (index) => {
-    const updatedTodos = this.state.todos.filter((_, i) => i !== index);
-    this.setState({ todos: updatedTodos });
+  const removeTask = (index) => {
+    const updatedTodos = todos.filter((_, i) => i !== index);
+    setTodos(updatedTodos);
   };
 
-  updateTask = (index) => {
-    const updatedTodos = this.state.todos.map((todo, i) => {
+  const updateTask = (index) => {
+    const updatedTodos = todos.map((todo, i) => {
       if (i === index) {
         return { ...todo, isCompleted: !todo.isCompleted };
       }
       return todo;
     });
-    this.setState({ todos: updatedTodos });
+    setTodos(updatedTodos);
   };
 
-  removeAllTasks = () => {
-    this.setState({ todos: [] });
+  const removeAllTasks = () => {
+    setTodos([]);
   };
 
-  render() {
-    return (
-      <div className="todo-app">
-        <h1>Todo List</h1>
-        <div className="todo-input">
-          <input
-            type="text"
-            placeholder="Enter a task"
-            value={this.state.task}
-            onChange={this.handleTaskChange}
-          />
-          <button onClick={this.addTask}>Add</button>
-        </div>
-        <ul>
-          {this.state.todos.map((todo, index) => (
-            <li key={index} className={todo.isCompleted ? "completed" : ""}>
-              {todo.task}
-              <button onClick={() => this.removeTask(index)}>Remove</button>
-              <button
-                onClick={() => this.updateTask(index)}
-                className="update-button"
-              >
-                {todo.isCompleted ? "Unmark" : "Mark"}
-              </button>
-            </li>
-          ))}
-        </ul>
-        {this.state.todos.length > 0 && (
-          <button onClick={this.removeAllTasks}>Remove All</button>
-        )}
+  return (
+    <div className="todo-app">
+      <h1>Todo List</h1>
+      <div className="todo-input">
+        <input
+          type="text"
+          placeholder="Enter a task"
+          value={task}
+          onChange={handleTaskChange}
+        />
+        <button onClick={addTask} className="add-button">
+          Add
+        </button>
       </div>
-    );
-  }
-}
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index} className={todo.isCompleted ? "completed" : ""}>
+            {todo.task}
+            <button onClick={() => removeTask(index)} className="remove-button">
+              Remove
+            </button>
+            <button
+              onClick={() => updateTask(index)}
+              className="update-button"
+            >
+              {todo.isCompleted ? "Unmark" : "Mark"}
+            </button>
+          </li>
+        ))}
+      </ul>
+      {todos.length > 0 && (
+        <button onClick={removeAllTasks} className="remove-all-button">
+          Remove All
+        </button>
+      )}
+    </div>
+  );
+};
 
 export default TodoApp;
